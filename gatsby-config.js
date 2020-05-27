46266fee2f45cf8b9368b51a82ef032eab4891c4
codypearce <codyapearce@gmail.com>
2020-05-26 22:19:04 -0700

    Set up RNW

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `React Universal`,
+    description: `Ecosystem of Universal React libraries, components, and tools that work on all platforms`,
+    author: `@codyapearce`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -26,9 +26,9 @@ module.exports = {
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
-    },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
+    }, // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
+    "gatsby-plugin-react-native-web",
   ],
 }
